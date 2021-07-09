c29c5c26f3e295d50470d56adc4de9e162761fe7
Jonathan <46301474+Z3NX-Y@users.noreply.github.com>
2021-07-09 17:53:57 +0200

    Update Generator.py

--- Generator.py
+++ Generator.py
@@ -1,1 +1,21 @@
+import random
+import string
 
+file = open
+
+def MakeKey():
+    k1 = ''.join(random.choice(string.ascii_uppercase + string.digits)
+                 for _ in range(5))
+    k2 = ''.join(random.choice(string.ascii_uppercase + string.digits)
+                 for _ in range(5))
+    k3 = ''.join(random.choice(string.ascii_uppercase + string.digits)
+                 for _ in range(5))
+    print(f'{k1}-{k2}-{k3}')
+    FinalCode = k1+"-"+k2+"-"+k3
+
+
+
+KeyCount = int(input("How many gift-cards do you want to generate? "))
+
+for key in range(KeyCount):
+    MakeKey()
